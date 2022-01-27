5a4ac9527f64c9db182b189ddcc725f33291bc53
Langer <bruno.langer@erzinger.com.br>
2022-01-27 12:50:52 -0300

    fix exposed secret

--- dist/auth.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authToken = exports.mongoUrl = void 0;
-exports.mongoUrl = "mongodb+srv://backend:puSbewHAr5X5IPJ4@turbo-train.ayywa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-exports.authToken = "";
